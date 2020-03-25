4ea5b2c52d37b8dc3d85d22b5da1143d52113a31
Ana Gomes <aninha.g.souza@hotmail.com>
2020-03-24 21:58:26 -0300

    Add files via upload

---
+++ Exemplo01/src/com/mvc/controlador/Controlador.java
@@ -0,0 +1,12 @@
+package com.mvc.controlador;
+
+import com.mvc.modelo.Modelo;
+
+public class Controlador {
+	
+	public boolean validarLogin(String usuario, String senha){
+		Modelo modelo = new Modelo();
+		return modelo.validarLogin(usuario,senha);
+	}
+
+}
---
+++ Exemplo01/src/com/mvc/modelo/Modelo.java
@@ -0,0 +1,12 @@
+package com.mvc.modelo;
+
+public class Modelo {
+
+	private String usuario = "dellead";
+	private String senha = "123456";
+	
+	public boolean validarLogin(String usuario, String senha) {
+		return this.usuario.equalsIgnoreCase(usuario) && this.senha.equalsIgnoreCase(senha)? true : false;
+	}
+	
+}
